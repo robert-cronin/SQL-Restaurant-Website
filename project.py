814f81d51e3353234397ee85c70903c83f993052
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2017-03-10 15:12:42 +1000

    remove bugs

--- project.py
+++ project.py
@@ -1,11 +1,12 @@
 # Flask framework
-from flask import flaskapp = Flask(__name__)
+from flask import Flask
+flaskapp = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
     return "Hello World"
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port=5000)
