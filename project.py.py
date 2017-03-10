270c528db20fd39bd6937881f776cf242f215a3c
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2017-03-10 15:07:59 +1000

    Add simple hello world test

--- project.py.py
+++ project.py.py
@@ -1,1 +1,11 @@
 # Flask framework
+from flask import flaskapp = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+
+if __name__ = '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port=5000)
